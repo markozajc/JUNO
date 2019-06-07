24962968a53f842d7ccf72b253d52360fe0837ab
Marko Zajc <marko.zajc@protonmail.com>
2019-06-07 19:58:05 +0200

    Create UnoRuleConflictException

---
+++ src/main/java/com/github/markozajc/juno/rules/UnoRuleConflictException.java
@@ -0,0 +1,26 @@
+package com.github.markozajc.juno.rules;
+
+public class UnoRuleConflictException extends RuntimeException {
+
+	private final UnoRule rule;
+	private final UnoRule conflicting;
+
+	public UnoRuleConflictException(UnoRule rule, UnoRule conflicting) {
+		this.rule = rule;
+		this.conflicting = conflicting;
+	}
+
+	@Override
+	public String getMessage() {
+		return this.rule.getClass().getSimpleName() + " conflicts with " + this.conflicting.getClass().getSimpleName();
+	}
+
+	public UnoRule getRule() {
+		return this.rule;
+	}
+
+	public UnoRule getConflicting() {
+		return this.conflicting;
+	}
+
+}
