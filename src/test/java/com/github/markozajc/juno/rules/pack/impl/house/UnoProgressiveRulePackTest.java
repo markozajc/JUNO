83c55633848d270a4d746276aeb7e66204e2b37d
Marko Zajc <marko.zajc@protonmail.com>
2019-06-07 23:47:18 +0200

    Create UnoProgressiveRulePackTest

---
+++ src/test/java/com/github/markozajc/juno/rules/pack/impl/house/UnoProgressiveRulePackTest.java
@@ -0,0 +1,64 @@
+package com.github.markozajc.juno.rules.pack.impl.house;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.github.markozajc.juno.cards.UnoCard;
+import com.github.markozajc.juno.cards.UnoCardColor;
+import com.github.markozajc.juno.cards.impl.UnoDrawCard;
+import com.github.markozajc.juno.cards.impl.UnoNumericCard;
+import com.github.markozajc.juno.piles.impl.UnoDiscardPile;
+
+class UnoProgressiveRulePackTest {
+
+	@Test
+	void testGetConsecutive() {
+		UnoDiscardPile discard = new UnoDiscardPile();
+		// Initializes the discard pile
+
+		discard.add(new UnoDrawCard());
+		discard.add(new UnoDrawCard());
+		discard.add(new UnoDrawCard());
+		discard.add(new UnoDrawCard());
+		discard.getCards().forEach(UnoCard::markOpen);
+		// Adds 4 open draw four cards
+
+		List<UnoDrawCard> consecutive = UnoProgressiveRulePack.getConsecutive(discard);
+		assertEquals(consecutive.size() * consecutive.get(0).getAmount(), 16);
+		// That is a consecutive draw of 16 cards
+
+		int drawMark = consecutive.get(0).getAmount();
+		for (UnoDrawCard drawCard : consecutive) {
+			assertEquals(drawMark, drawCard.getAmount());
+			assertTrue(drawCard.isOpen());
+		}
+		// Checks the relevance of the cards in the returned consecutive draw cards list
+
+		discard.getCards().get(3).markClosed();
+		// Sets the bottom card as closed
+
+		consecutive = UnoProgressiveRulePack.getConsecutive(discard);
+		assertEquals(consecutive.size() * consecutive.get(0).getAmount(), 12);
+		// That is a consecutive draw of 12 cards
+
+		discard.add(new UnoNumericCard(UnoCardColor.RED, 0));
+		// Adds an irrelevant color to the top
+
+		consecutive = UnoProgressiveRulePack.getConsecutive(discard);
+		assertEquals(consecutive.size() * consecutive.get(0).getAmount(), 0);
+		// That is a consecutive draw of 0 cards
+
+		discard.add(new UnoDrawCard(UnoCardColor.RED));
+		discard.getTop().markOpen();
+		// Adds a single open draw two card
+
+		consecutive = UnoProgressiveRulePack.getConsecutive(discard);
+		assertEquals(consecutive.size() * consecutive.get(0).getAmount(), 2);
+		// That is a consecutive draw of 2 cards
+	}
+
+}
