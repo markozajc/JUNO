5d28b7036d5f3ec2c0acfc7d112f3c32b5f16f9e
Marko Zajc <marko@zajc.eu.org>
2022-06-18 01:52:53 +0200

    Remove deprecated code

--- src/main/java/com/github/markozajc/juno/utils/UnoUtils.java
+++ src/main/java/com/github/markozajc/juno/utils/UnoUtils.java
@@ -8,9 +8,6 @@ import java.util.stream.Collectors;
 import com.github.markozajc.juno.cards.*;
 import com.github.markozajc.juno.cards.impl.*;
 import com.github.markozajc.juno.cards.impl.UnoActionCard.UnoFlowAction;
-import com.github.markozajc.juno.game.UnoGame;
-import com.github.markozajc.juno.hands.UnoHand;
-import com.github.markozajc.juno.piles.impl.UnoDiscardPile;
 
 /**
  * A class containing various utilities, mostly used to process lists of
@@ -21,96 +18,6 @@ import com.github.markozajc.juno.piles.impl.UnoDiscardPile;
 public class UnoUtils {
 
 	private UnoUtils() {}
-
-	/**
-	 * Finds all possible cards that can be placed on a certain {@link UnoCard} from a
-	 * {@link List} of cards. This implements all standard UNO rules (same color for all
-	 * {@link UnoCard}s, same number for all {@link UnoNumericCard}s or same action for
-	 * {@link UnoActionCard}) as well as the progressive UNO rule (same quantity for
-	 * {@link UnoDrawCard}s). This is intended to be utilized by {@link UnoHand}s to be
-	 * able to decide between their available cards better and more easily and by
-	 * {@link UnoGame} implementations to prevent cheating - accidental or not.
-	 *
-	 * @param targetCard
-	 *            the target card (eg. the top {@link UnoCard} of the
-	 *            {@link UnoDiscardPile})
-	 * @param cards
-	 *            the {@link Collection} of cards to analyze (eg. {@link UnoHand}'s
-	 *            cards)
-	 *
-	 * @return a {@link List} of all possible cards from {@code cards} can be placed on
-	 *         the {@code targetCard}.
-	 *
-	 * @deprecated This method only supports a hard-coded set of rules. Use
-	 *             {@link UnoRuleUtils#combinedPlacementAnalysis(UnoCard, Collection, com.github.markozajc.juno.rules.pack.UnoRulePack, UnoHand)}
-	 *             instead.
-	 */
-	@Deprecated
-	public static List<UnoCard> analyzePossibleCards(UnoCard targetCard, Collection<UnoCard> cards) {
-		List<UnoCard> result = new ArrayList<>();
-
-		if (targetCard instanceof UnoDrawCard && ((UnoDrawCard) targetCard).isOpen()) {
-			UnoDrawCard castTop = (UnoDrawCard) targetCard;
-
-			result.addAll(UnoUtils.filterKind(UnoDrawCard.class, cards)
-				.stream()
-				.filter(c -> c.getAmount() == castTop.getAmount())
-				.collect(Collectors.toList()));
-
-			return result;
-		}
-		// Adds all allowed draw cards. This is an exception as it only returns draw cards in
-		// case the other player placed a draw card (a progressive UNO thing, similar to the
-		// "check" state in Chess)
-
-		if (targetCard instanceof UnoWildCard && targetCard.getColor() == UnoCardColor.WILD)
-			return Collections.emptyList();
-		// No card can be placed on an unset wild card
-
-		if (targetCard instanceof UnoNumericCard) {
-			UnoNumericCard castTop = (UnoNumericCard) targetCard;
-
-			result.addAll(UnoUtils.filterKind(UnoNumericCard.class, cards)
-				.stream()
-				.filter(c -> c.getNumber() == castTop.getNumber())
-				.collect(Collectors.toList()));
-		}
-		// Adds all allowed numerical cards.
-
-		else if (targetCard instanceof UnoActionCard) {
-			UnoActionCard castTop = (UnoActionCard) targetCard;
-
-			result.addAll(UnoUtils.filterKind(UnoActionCard.class, cards)
-				.stream()
-				.filter(c -> c.getFlowAction() == castTop.getFlowAction())
-				.collect(Collectors.toList()));
-		}
-		// Adds all allowed action cards
-
-		else if (targetCard instanceof UnoDrawCard && !((UnoDrawCard) targetCard).isOpen()) {
-			UnoDrawCard castTop = (UnoDrawCard) targetCard;
-
-			result.addAll(UnoUtils.filterKind(UnoDrawCard.class, cards)
-				.stream()
-				.filter(c -> c.getAmount() == castTop.getAmount())
-				.collect(Collectors.toList()));
-		}
-		// Adds all allowed draw cards.
-
-		result.addAll(UnoUtils.getColorCards(UnoCardColor.WILD, cards));
-		// Adds all wild-colored cards. Wild cards can be placed on anything.
-
-		if (targetCard.getColor() != UnoCardColor.WILD) {
-			for (UnoCard card : UnoUtils.getColorCards(targetCard.getColor(), cards)) {
-				if (!result.contains(card))
-					result.add(card);
-			}
-		}
-		// Adds all non-wild-colored cards of the same color (that haven't been added
-		// already)
-
-		return result;
-	}
 
 	/**
 	 * Analyzes cards and returns them in an {@link Entry} of {@link UnoCardColor}
