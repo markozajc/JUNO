7ea48834bd95aa8c5325e48f597ecc55a1d801d3
Marko Zajc <marko@zajc.eu.org>
2022-09-03 18:34:36 +0200

    Make FoeWinsOnEndRequest pass-through in games with more than 2 players

--- src/main/java/com/github/markozajc/juno/rules/pack/impl/house/UnoFoeWinsOnEndRequestPack.java
+++ src/main/java/com/github/markozajc/juno/rules/pack/impl/house/UnoFoeWinsOnEndRequestPack.java
@@ -1,9 +1,10 @@
 package com.github.markozajc.juno.rules.pack.impl.house;
 
+import static com.github.markozajc.juno.game.UnoWinner.UnoEndReason.REQUESTED;
+
 import javax.annotation.Nonnull;
 
 import com.github.markozajc.juno.game.*;
-import com.github.markozajc.juno.game.UnoWinner.UnoEndReason;
 import com.github.markozajc.juno.players.UnoPlayer;
 import com.github.markozajc.juno.rules.pack.UnoRulePack;
 import com.github.markozajc.juno.rules.types.UnoGameFlowRule;
@@ -11,7 +12,9 @@ import com.github.markozajc.juno.rules.types.flow.UnoFinishConclusion;
 
 /**
  * By default calling {@link UnoGame#endGame()} will result in a draw. Apply this
- * rule pack if you want the other player to win in such case.
+ * rule pack if you want the other player to win in such case.<br>
+ * <b>NOTE: this only works in games with two players. It will do nothing
+ * otherwise.</b>
  *
  * @author Marko Zajc
  *
@@ -31,7 +34,7 @@ public class UnoFoeWinsOnEndRequestPack {
 		@Override
 		public UnoFinishConclusion finishPhase(UnoWinner winner, UnoGame game) {
 			UnoPlayer lastPlayer = game.getLastPlayer();
-			if (winner.getEndReason() == UnoEndReason.REQUESTED && lastPlayer != null)
+			if (game.getPlayers().size() == 2 && winner.getEndReason() == REQUESTED && lastPlayer != null)
 				return new UnoFinishConclusion(game.getNextPlayer());
 			else
 				return UnoFinishConclusion.NOTHING;
