8cc4d623b47dbef0f84e9284f76436b6f8e0aacd
Marko Zajc <marko@zajc.eu.org>
2023-08-09 00:24:26 +0200

    Update the licensing information

--- src/main/java/org/eu/zajc/juno/piles/impl/UnoDiscardPile.java
+++ src/main/java/org/eu/zajc/juno/piles/impl/UnoDiscardPile.java
@@ -1,3 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+/*
+ * JUNO, the UNO library for Java 
+ * Copyright (C) 2019-2023 Marko Zajc
+ *
+ * This program is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this
+ * program. If not, see <https://www.gnu.org/licenses/>.
+ */
 package org.eu.zajc.juno.piles.impl;
 
 import static java.util.Collections.emptyList;
