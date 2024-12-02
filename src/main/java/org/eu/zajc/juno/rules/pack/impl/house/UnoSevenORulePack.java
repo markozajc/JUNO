3c48fe26b547cc6798d145f40b1ea09326aef87c
Marko Zajc <marko@zajc.tel>
2024-12-02 11:34:36 +0100

    Update the copyright headers

--- src/main/java/org/eu/zajc/juno/rules/pack/impl/house/UnoSevenORulePack.java
+++ src/main/java/org/eu/zajc/juno/rules/pack/impl/house/UnoSevenORulePack.java
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 /*
  * JUNO, the UNO library for Java 
- * Copyright (C) 2019-2023 Marko Zajc
+ * Copyright (C) 2019-2024 Marko Zajc
  *
  * This program is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
